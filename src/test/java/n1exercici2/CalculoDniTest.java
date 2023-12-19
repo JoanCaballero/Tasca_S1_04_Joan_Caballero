5d34078934bf043854a77ae14338e0e5c7fa7a78
JoanCaballero <caballero.portoles.joan@gmail.com>
2023-12-19 12:56:33 +0100

    Tasca 4 n1 corregit

--- src/test/java/n1exercici2/CalculoDniTest.java
+++ src/test/java/n1exercici2/CalculoDniTest.java
@@ -5,20 +5,18 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class JUnit {
+class CalculoDniTest {
     @ParameterizedTest
     @ValueSource(ints = {23156754, 26859758, 23156498, 21764253, 64523148, 78945645, 87954640, 21305476, 35968455, 66523455, 87963420}) // 10 DNI
     void esCorrecte(int DNI){
+        char c = getLetra(DNI);
         CalculoDni calculo = new CalculoDni(DNI);
+        assertEquals(c, calculo.getC());
+    }
+    private char getLetra(int DNI){
         char[] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',
                 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
-        boolean trobat = false;
-        for(char c : letras){
-            if (calculo.getC() == c) {
-                trobat = true;
-                break;
-            }
-        }
-        assertTrue(trobat);
+        int residu = DNI%23;
+        return letras[residu];
     }
 }
--- src/test/java/org/n1exercici1/MesosTest.java
+++ src/test/java/org/n1exercici1/MesosTest.java
@@ -1,15 +1,17 @@
 package org.n1exercici1;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MesosTest {
-    @Test
-    public void MesosT(){
-        ArrayList<String> arrayMesos = new ArrayList<>();
+public class MesosTest {
+    private static ArrayList<String> arrayMesos;
+    @BeforeAll
+    public static void MesosArray(){
+        arrayMesos = new ArrayList<>();
         arrayMesos.add("Gener");
         arrayMesos.add("Febrer");
         arrayMesos.add("Març");
@@ -22,8 +24,19 @@ class MesosTest {
         arrayMesos.add("Octubre");
         arrayMesos.add("Novembre");
         arrayMesos.add("Desembre");
+    }
+    @Test
+    public void MesosSize(){
         assertEquals(12, arrayMesos.size());
+    }
+
+    @Test
+    public void MesosNotNull(){
         assertNotNull(arrayMesos);
+    }
+
+    @Test
+    public void MesosAtSeven(){
         assertEquals("Agost", arrayMesos.get(7));
     }
 }
