852f93e7fb7b3bd66d10d8bbbd3fa47f9ffcae70
JoanCaballero <caballero.portoles.joan@gmail.com>
2023-12-20 10:22:46 +0100

    Tasca 4 n1 corregit

--- src/test/java/n1exercici2/CalculoDniTest.java
+++ src/test/java/n1exercici2/CalculoDniTest.java
@@ -1,22 +1,28 @@
 package n1exercici2;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculoDniTest {
     @ParameterizedTest
-    @ValueSource(ints = {23156754, 26859758, 23156498, 21764253, 64523148, 78945645, 87954640, 21305476, 35968455, 66523455, 87963420}) // 10 DNI
-    void esCorrecte(int DNI){
-        char c = getLetra(DNI);
-        CalculoDni calculo = new CalculoDni(DNI);
-        assertEquals(c, calculo.getC());
-    }
-    private char getLetra(int DNI){
-        char[] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',
-                'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
-        int residu = DNI%23;
-        return letras[residu];
+    @CsvSource ({
+            "23156754,D",
+            "26859758,J",
+            "23156498,Y",
+            "21764253,C",
+            "64523148,Y",
+            "78945645,P",
+            "87954640,H",
+            "21305476,R",
+            "35968455,C",
+            "66523455,A",
+            "87963420,N"})
+    void esCorrecto(int dni, char caracterCorrecte){
+        CalculoDni calculoDni = new CalculoDni(dni);
+        assertEquals(caracterCorrecte, calculoDni.getC());
     }
 }
